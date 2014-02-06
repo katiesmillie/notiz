c13735811288aef3b01d44461ed08f64977e3ce1
Katie Smillie <katiesmillie@gmail.com>
2014-02-05 23:22:15 -0500

    change email subject

--- app/mailers/notes_mailer.rb
+++ app/mailers/notes_mailer.rb
@@ -5,7 +5,7 @@ class NotesMailer < ActionMailer::Base
     @sender_email = sender_email
     @message = message
     @days = days
-    mail(:to => recipient_email, :from => sender_email, :subject => "You receieved a note from the past")
+    mail(:to => recipient_email, :from => sender_email, :subject => "Someone sent you a note from the past")
   end
 
 end
