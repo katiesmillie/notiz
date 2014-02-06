fe005916049e89e6cd5e6eec74259c7755053da2
Katie Smillie <katiesmillie@gmail.com>
2014-02-05 23:10:08 -0500

    stupid katie .deliver

--- app/models/note.rb
+++ app/models/note.rb
@@ -9,7 +9,7 @@ end
 def self.mail_notes
 	Note.all.each do |n|
 		if n.send_date == Date.today
-			NotesMailer.notes_email(n.recipient_email, n.sender_email, n.body, n.days)
+			NotesMailer.notes_email(n.recipient_email, n.sender_email, n.body, n.days).deliver
 		else
 		end
 	end
