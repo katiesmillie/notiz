580053f6c6d5312e9ff18bc424d70d88f0f23030
Katie Smillie <katiesmillie@gmail.com>
2015-02-18 17:33:07 -0500

    comment out in application.rb test

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Notiz
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+    # config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
   end
 end
 
